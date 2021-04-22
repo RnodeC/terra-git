392e85f2d1575c262544d6d52bb4965b4d1e15d9
Jeff Owens <ojeffg@pm.me>
2021-04-21 22:26:36 -0400

    valcheck pt 2

--- terra-watch/valcheck.sh
+++ terra-watch/valcheck.sh
@@ -3,14 +3,19 @@
 while [[ $# -gt 0 ]]; do 
   key="$1"
   case $key in 
-    -v)
+    -m)
       shift
-      VALIDATOR="$1"
+      MONIKER="$1"
       shift
       ;;
-    -b)
+    -l)
       shift
-      VALIDATORBECH="$1"
+      LCDHOST="$1"
+      shift
+      ;;
+    -p)
+      shift
+      LCDPORT="$1"
       shift
       ;;
     *)
@@ -20,46 +25,51 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
+# default lcd values - assuming localhost.  (I like to ssh port forward client to validator or sentry's lcd)
 LCDHOST=${LCDHOST:=localhost}
 LCDPORT=${LCDPORT:=1317}
-[ -z $VALIDATOR ] && echo "[ERROR] Must provide your validator address (-v)" && echo "hint: find this value with this command on your validator node: terrad tendermint show-validator" && exit 1
-[ -z $VALIDATORBECH ] && echo "[ERROR] Must provide your validator bech address (-b)" && echo "hint: find this value with this command whereever you validator address is stored: terracli keys show -a --bech val <walletname>" && exit 1
+
+[ -z $MONIKER ] && echo "[ERROR] Must provide moniker name (-m)" && exit 1
 
 
-VALIDATOR=terravalconspub1zcjduepqvmsjtatpwg5yr4lsq296zudmvrr3ra9tln9paap2tmzg8sa0ytaqm5c428 #terrad tendermint show-validator
-VALIDATORBECH=terravaloper1s5cyxwl36atv65m03pumrp9alhn2xdtzw5ug4l #terracli keys show -a --bech val <walletname>
+# get addresses from moniker
+CONSENSUS_PUBKEY=$(curl -s ${LCDHOST}:${LCDPORT}/staking/validators | jq --arg moniker "$MONIKER" '.result | .[] | select (.description.moniker == $moniker) | .consensus_pubkey' -r)
+VALIDATORBECH=$(curl -s ${LCDHOST}:${LCDPORT}/staking/validators | jq --arg moniker "$MONIKER" '.result | .[] | select (.description.moniker == $moniker) | .operator_address' -r)
 
-# get moniker name
-MONIKER=$(curl -s ${LCDHOST}:1317/staking/validators/${VALIDATORBECH} | jq '.result.description.moniker' -r)
+[ -z $CONSENSUS_PUBKEY ] && echo "[ERROR] CONSENSUS_PUBKEY not found" && exit 1
+[ -z $VALIDATORBECH ] && echo "[ERROR] VALIDATORBECH not found" && exit 1
+
 echo; echo "[INFO] Checking on validator: $MONIKER"
+echo -e "\tCONSENSUS_PUBKEY: $CONSENSUS_PUBKEY"
+echo -e "\tOPERATOR_ADDRESS: $VALIDATORBECH"
 
 
 # make sure we are not jailed
-JAILED=$(curl -s ${LCDHOST}:1317/staking/validators/${VALIDATORBECH} | jq '.result.jailed' -r)
+JAILED=$(curl -s ${LCDHOST}:${LCDPORT}/staking/validators/${VALIDATORBECH} | jq '.result.jailed' -r)
 if [ ! "$JAILED" = "false" ]; then 
-echo; echo "[WARN] You are not not jailed!  Run: curl -s ${LCDHOST}:1317/staking/validators/${VALIDATORBECH} |jq"
+echo; echo "[WARN] You are not not jailed!  Run: curl -s ${LCDHOST}:${LCDPORT}/staking/validators/${VALIDATORBECH} |jq"
 exit 1
 fi
 
 
 # how much is staked with us
-STAKE=$(curl -s ${LCDHOST}:1317/staking/validators/${VALIDATORBECH} | jq '.result.tokens' -r)
+STAKE=$(curl -s ${LCDHOST}:${LCDPORT}/staking/validators/${VALIDATORBECH} | jq '.result.tokens' -r)
 echo; echo "$MONIKER has ${STAKE}uluna staked"
 
 
 # how many blocks have we missed
-MISSEDBLOCKS=$(curl -s ${LCDHOST}:1317/slashing/validators/${VALIDATOR}/signing_info| jq '.result.missed_blocks_counter' -r)
+MISSEDBLOCKS=$(curl -s ${LCDHOST}:${LCDPORT}/slashing/validators/${CONSENSUS_PUBKEY}/signing_info| jq '.result.missed_blocks_counter' -r)
 echo; echo "${MONIKER} has missed $MISSEDBLOCKS blocks during this slashing period"
 
 
 # vote periods missed in this oracle slash window
-MISSEDVOTES=$(curl -s ${LCDHOST}:1317/oracle/voters/${VALIDATORBECH}/miss |jq '.result' -r)
+MISSEDVOTES=$(curl -s ${LCDHOST}:${LCDPORT}/oracle/voters/${VALIDATORBECH}/miss |jq '.result' -r)
 echo; echo "${MONIKER} has missed ${MISSEDVOTES} oracle votes during this slashing period"
 
 
 
 # how much luna we got in rewards
-ULUNARWDS=$(curl -s ${LCDHOST}:1317/distribution/validators/${VALIDATORBECH}/rewards |jq '.result | .[] | select(.denom == "uluna") | .amount' -r)
+ULUNARWDS=$(curl -s ${LCDHOST}:${LCDPORT}/distribution/validators/${VALIDATORBECH}/rewards |jq '.result | .[] | select(.denom == "uluna") | .amount' -r)
 echo; echo "${MONIKER} has ${ULUNARWDS}uluna ready to be claimed"
 
 
